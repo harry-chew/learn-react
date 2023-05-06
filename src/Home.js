826d6f70bd31075232bada43a0ee52e01285a724
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-06 14:49:45 +0100

    feat!: separated the components

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <div className="content">
+                <h1>App Component</h1>
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;

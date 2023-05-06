4d4a5d3d0ec6ca35434a78cafd50b3bd040d93cb
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-06 21:45:06 +0100

    feat: styling
    deleted all styling from template
    added the needed style for what we got so far

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,6 @@ const Home = () => {
     return ( 
         <div className="home">
             <div className="content">
-                <h1>App Component</h1>
             </div>
         </div>
      );

61f480eb96d3bee76d7ccdec53ae187105504612
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-07 11:44:21 +0100

    feat: useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -8,20 +8,23 @@ const Home = () => {
         {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
     ]);
 
+    const [name, setName] = useState('mario');
+
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(blogs);
-    });
+        console.log('name changed: ' + name);
+    }, [name]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!"/>
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
         </div>
      );
 }

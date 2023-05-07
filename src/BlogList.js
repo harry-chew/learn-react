0bf46e5e415c236431fec8f46b84c7af1b47f425
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-07 07:57:55 +0100

    feat: using functions in props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,4 @@
-const BlogList = ({ blogs, title }) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
+const BlogList = ({ blogs, title, handleDelete }) => {
 
     return ( 
         <div className="blog-list">
@@ -9,6 +7,7 @@ const BlogList = ({ blogs, title }) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>

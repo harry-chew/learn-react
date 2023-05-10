a2ff1b1aa5c5c0d4aa9910a133396857df81e583
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-10 12:14:54 +0100

    feat!: using json server and useEffect hook to pull data into page

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,24 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const[blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
-    ]);
+    const[blogs, setBlogs] = useState( null );
 
     const [name, setName] = useState('mario');
 
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
     useEffect(() => {
-        console.log('name changed: ' + name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        })
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>{name}</p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>
      );
 }

dab169ea6d37d475179d6f466f456101bb428a4f
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-12 12:08:09 +0100

    feat: conditional loading

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const[blogs, setBlogs] = useState( null );
-
-    const [name, setName] = useState('mario');
-
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
@@ -14,11 +12,13 @@ const Home = () => {
         })
         .then(data => {
             setBlogs(data);
+            setIsPending(false);
         })
     }, []);
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>
      );

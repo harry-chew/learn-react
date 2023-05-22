224ed2f0cb7216546f45653facdfb25eb6499c7e
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-23 00:34:54 +0100

    feat: react router
    installed react-router-dom package and setup for content switching

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 

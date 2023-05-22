0cc9dee3c114b86d799edeeacbc65fb7ac4303dd
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-22 23:55:26 +0100

    feat: useState error hook

--- src/Home.js
+++ src/Home.js
@@ -4,26 +4,30 @@ import BlogList from './BlogList';
 const Home = () => {
     const[blogs, setBlogs] = useState( null );
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
-            if(res.ok) {
-                return res.json();  
+            if(!res.ok) {
+                throw Error('Could not fetch the data for that resource');
             }
-            
+            return res.json();  
         })
         .then(data => {
             setBlogs(data);
             setIsPending(false);
+            setError(null);
         })
         .catch(err => {
-            console.log(err.message);
+            setError(err.message);
+            setIsPending(false);
         })
     }, []);
 
     return ( 
         <div className="home">
+            {error && <div>{ error }</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>

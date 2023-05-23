1f64ec97c12407f4223a3506762135b19886a7f9
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-23 11:41:04 +0100

    update:  not needed with this version of react-router-dom

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,23 +7,27 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
-        .then(res => {
-            if(!res.ok) {
-                throw Error('Could not fetch the data for that resource');
-            }
-            return res.json();  
-        })
-        .then(data => {
-            setData(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setError(err.message);
-            setIsPending(false);
-        })
-    }, [url]);
+        const abortCont = new AbortController();
+
+            fetch(url, { signal: abortCont.signal })
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('Could not fetch the data for that resource');
+                }
+                return res.json();  
+            })
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                    setError(err.message);
+                    setIsPending(false);
+            });
+
+        }, [url]);
+    
 
     return { data, isPending, error };
 }

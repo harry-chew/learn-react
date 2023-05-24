4f607bbe7c2efad2f02c24ff977cf5a55be214e6
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-24 11:19:42 +0100

    feat: submit event

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,16 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        console.log(blog);
+    };
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="">Blog title:</label>
                 <input type="text" 
                     required 

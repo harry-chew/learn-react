acebc08dbcbd929e0922ed2af28e8ac9f71f1dc2
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-29 09:09:14 +0100

    feat: useHistory to redirect user

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,7 +20,10 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added');
             setIsPending(false);
+            history.push('/');
         });
+
+        
     };
     return ( 
         <div className="create">

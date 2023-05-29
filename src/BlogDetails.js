2fc43473018377ae3166b8046da762eef9213384
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2023-05-29 09:16:20 +0100

    feat: deleting blog from blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,18 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
-
 const BlogDetails = () => {
 
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        });
+    }
 
     return ( 
         <div className="blog-details">
@@ -18,6 +26,7 @@ const BlogDetails = () => {
                     </div>
                     
                     <div className="blog-body">{ blog.body }</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
